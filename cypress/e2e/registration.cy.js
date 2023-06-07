b6aad151c311cf1ce9fe8786822fb39dcc6eb0a1
claudia <claudia.iacob@hotmail.com>
2023-06-07 16:14:51 +0200

    Boton escondido

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -4,32 +4,58 @@ describe('Registro de usuario', () => {
     cy.visit('/index.html');
   });
 
-  it('Debe mostrar error si el correo electrónico no es válido', () => {
-    cy.get('#email').type('correo-invalido');
-    cy.get('#password').type('contraseña');
-    cy.get('#confirm-password').type('contraseña');
-    cy.get('#register-btn').click();
-    cy.get('#error-message').should('have.text', 'El correo no es valido');
-  });
+it('El formulario de registro contiene elementos de entrada para correo electrónico, contraseña y repetición de contraseña', () => {
+ 
 
-  it('Debe mostrar error si las contraseñas no coinciden', () => {
-    cy.get('#email').type('correo');
-    cy.get('#password').type('contraseña');
-    cy.get('#confirm-password').type('contraseña-invalida');
-    cy.get('#register-btn').click();
-    cy.get('#error-message1').should('have.text', 'Las contraseñas no coinciden');
-  });
+  // Verifica que los elementos de entrada existan utilizando los atributos "id"
+  cy.get('#email').should('exist');
+  cy.get('#password').should('exist');
+  cy.get('#confirm-password').should('exist');
+});
 
-  it('Debe mostrar error si algun campo esta vacío', () => {
-    cy.get('#register-btn').click();
-    cy.get('#error-message2').should('have.text', 'Todos los campos son obligatorios');
-  });
 
-  it('Debe permitir el registro si los campos son válidos', () => {
-    cy.get('#email').type('correo@example.com');
-    cy.get('#password').type('contraseña');
-    cy.get('#confirm-password').type('contraseña');
-    cy.get('#register-btn').click();
-    cy.get('#success-message').should('have.text', 'Se ha registrado correctamente');
-  });
+it('Muestra mensaje de advertencia y oculta el botón cuando las contraseñas no coinciden', () => {
+
+  // Ingresa una contraseña en el campo "Contraseña"
+  cy.get('#password').type('password');
+
+  // Ingresa una contraseña diferente en el campo "Repetir contraseña"
+  cy.get('#confirm-password').type('differentpassword');
+
+  // Verifica que aparezca el mensaje de advertencia
+  cy.get('#password-message').should('be.visible');
+
+  // Verifica que el botón no esté visible
+  cy.get('#register-btn').should('have.css', 'display', 'none');
+});
+
+
+  // it('Debe mostrar error si el correo electrónico no es válido', () => {
+  //   cy.get('#email').type('correo-invalido');
+  //   cy.get('#password').type('contraseña');
+  //   cy.get('#confirm-password').type('contraseña');
+  //   cy.get('#register-btn').click();
+  //   cy.get('#error-message').should('have.text', 'El correo no es valido');
+  // });
+
+  // it('Debe mostrar error si las contraseñas no coinciden', () => {
+  //   cy.get('#email').type('correo');
+  //   cy.get('#password').type('contraseña');
+  //   cy.get('#confirm-password').type('contraseña-invalida');
+  //   cy.get('#register-btn').click();
+  //   cy.get('#password-message').should('have.text', 'Las contraseñas no coinciden');
+  // });
+
+  // it('Debe mostrar error si algun campo esta vacío', () => {
+  //   cy.get('#register-btn').click();
+  //   cy.get('#error-message2').should('have.text', 'Todos los campos son obligatorios');
+  // });
+
+  // it('Debe permitir el registro si los campos son válidos', () => {
+  //   cy.get('#email').type('correo@example.com');
+  //   cy.get('#password').type('contraseña');
+  //   cy.get('#confirm-password').type('contraseña');
+  //   cy.get('#register-btn').click();
+  //   cy.get('#success-message').should('have.text', 'Se ha registrado correctamente');
+  // });
 });
---
+++ js/main.js
@@ -0,0 +1,4 @@
+const password = document.getElementById('password');
+const repeatPassword = document.getElementById('confirm-password');
+const sendBtn = document.getElementById('register-btn');
+const passwordMessage = document.getElementById('password-message');
